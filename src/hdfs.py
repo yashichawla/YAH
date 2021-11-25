1b6c0743b38cca15116962dcacb45449f1357b48
yashi <yashichawla1@gmail.com>
2021-11-26 03:34:29 +0530

    run- output dir, hashing, rm-block/data info

--- src/hdfs.py
+++ src/hdfs.py
@@ -34,7 +34,6 @@ def load_config_from_json(filepath):
             else:
                 exec(f"args.{key.upper()} = '{value}'")
         except:
-            # print(key, value)
             pass
 
 
@@ -46,25 +45,20 @@ def load_args():
     args = parser.parse_args()
     load_config_from_json(args.CONFIG)
 
-    # TODO
     if args.NUM_LOAD == 0:
         print("DFS has been loaded for the first time. Please format using the `format` command")
         _input = None
         while _input != "format":
             _input = input("(hdfs) > ")
             if _input == "format":
-                format_namenode_datanode()
-                break
+                format_namenode_datanode(quit=False)
             else:
                 print("Please enter `format` to format the DFS")
-
-    # args.__dict_ is causing issues, need another way to load, update and put. it is getting datanode +namenode in config.
 
     args.NUM_LOAD += 1
     with open(args.DFS_SETUP_CONFIG, "w") as f:
         dfs_setup_config_dict = dict()
         dfs_setup_config_dict.update(args.__dict__)
-        # dfs_setup_config_dict = args.__dict__
         try:
             del dfs_setup_config_dict["FILESYSTEM"]
         except:
@@ -81,6 +75,9 @@ def load_args():
         dfs_setup_config_dict["NUM_LOAD"] = args.NUM_LOAD
         json.dump(dfs_setup_config_dict, f, indent=4)
 
+    if args.NUM_LOAD == 1:
+        exit(0)
+
     with open(Path(args.PATH_TO_NAMENODES).joinpath(args.PRIMARY_NAMENODE_NAME).joinpath(
             args.FILESYSTEM_INFO_FILENAME), 'r') as f:
         args.FILESYSTEM = json.load(f)
@@ -93,12 +90,6 @@ def load_args():
 
     with open(Path(args.PATH_TO_NAMENODES).joinpath(args.PRIMARY_NAMENODE_NAME).joinpath(args.FILE_INFO_FILENAME), 'r') as f:
         args.FILE_INFO = json.load(f)
-
-    # with open(Path(args.NAMENODE_LOG_PATH), "w") as f:
-    #     f.write(f"")
-    #     f.close()
-
-    # print(args)
 
 
 def run_mapper(mapper_filename, input_filename, index):
@@ -115,17 +106,23 @@ def aggregate_and_sort(outputs):
     return output
 
 
-def run_reducer(reducer_filename, outputs, output_filename):
+def run_reducer(reducer_filename, outputs, output_dir_name):
     command = f"echo -e '{outputs}' | python3 {reducer_filename}"
     output = os.popen(command)
-    with open('job_output', 'w') as f:
+
+    with open('output', 'w') as f:
         f.write(output.read().strip())
-    put('job_output', output_filename)
-    Path('job_output').unlink()
+    with open('status', 'w') as f:
+        f.write("JOB COMPLETED SUCCESSFULLY")
+
+    mkdir(output_dir_name)
+    put('output', f"{output_dir_name}/output.txt")
+    put('status', f"{output_dir_name}/status.txt")
+    Path('output').unlink()
+    Path('status').unlink()
 
 
-def format_namenode_datanode(*vargs):
-    # TODO
+def format_namenode_datanode(quit=True, *vargs):
     namenode_path = Path(args.PATH_TO_NAMENODES)
     shutil.rmtree(str(Path(namenode_path).joinpath(
         args.NAMENODE_CHECKPOINTS_PATH)))
@@ -172,6 +169,10 @@ def format_namenode_datanode(*vargs):
     update_namenode_datanode_info_local()
     update_secondary_namenode()
     create_namenode_checkpoints()
+
+    print("Name Node and Data Nodes formatted successfully. Please restart.")
+    if quit:
+        exit(0)
 
 
 def check_namenode_and_metadata_exists():
@@ -380,7 +381,7 @@ def create_file_in_hdfs(destination_file_path):
         return False
 
 
-def choose_datanode(mode="least"):
+def choose_datanode(mode="least", block_path=None, replica_count=None):
     all_datanodes = list(args.DATANODE_INFO.keys())
     datanode_capacities = list(
         map(lambda x: len(args.DATANODE_INFO[x]), all_datanodes))
@@ -396,8 +397,11 @@ def choose_datanode(mode="least"):
         elif mode == "random":
             return random.choice(available_datanodes)[0]
         elif mode == 'hashing':
-            # TODO
-            pass
+            with open(block_path, 'r') as f:
+                block_hash = f.read()
+            block_hash = hash(block_hash) + replica_count
+            block_hash = block_hash % len(available_datanodes)
+            return available_datanodes[block_hash][0]
 
 
 def mkdir(*vargs):
@@ -456,6 +460,24 @@ def ls(*vargs):
         return False
 
 
+def remove_file_from_datanodes(file_id):
+    num_blocks = args.FILE_INFO[file_id]['num_blocks']
+    block_ids = [f"{file_id}__{i}" for i in range(1, num_blocks + 1)]
+    for b_id in block_ids:
+        del args.BLOCK_INFO[b_id]
+    update_namenode_block_info()
+    for datanode_id in range(args.NUM_DATANODES):
+        datanode_id = f"DATANODE{datanode_id}"
+        args.DATANODE_INFO[datanode_id] = [
+            b_id for b_id in args.DATANODE_INFO[datanode_id] if b_id not in block_ids]
+        datanode_path = Path(args.PATH_TO_DATANODES).joinpath(datanode_id)
+        blocks_in_datanode = list(datanode_path.glob('**/*'))
+        for block in blocks_in_datanode:
+            if str(block) in block_ids:
+                block.unlink()
+    update_namenode_datanode_info()
+
+
 def rm(*vargs):
     path = vargs[0]
     if check_path_exists_in_hdfs(path):
@@ -470,6 +492,7 @@ def rm(*vargs):
                 del current[components[-1]]
                 update_namenode_filesystem_info()
                 file_id = get_file_id_from_hdfs_file_path(path)
+                remove_file_from_datanodes(file_id)
                 del args.FILE_INFO[file_id]
                 update_namenode_file_info()
                 return True
@@ -493,8 +516,12 @@ def rmdir(*vargs):
         for component in components[:-1]:
             if component in current:
                 current = current[component]
+        # /test1/test2/test3/new.txt components[-1]=test3, current=test2
         if components[-1] in current:
             if not current[components[-1]] is None:
+                # traverse file info, for every file find file path, if it starts with path, then call rm for each of those files.
+                # CANT string comp. Split, components, find sublist in file info.
+                # check sublist of file info path element wise.
                 del current[components[-1]]
                 update_namenode_filesystem_info()
                 return True
@@ -535,7 +562,6 @@ def put(*vargs):
         return False
     else:
         check_file_created = create_file_in_hdfs(destination_file_path)
-        # print(check_file_created, args.FILESYSTEM)
         if check_file_created:
             Path("./temp").mkdir(parents=True, exist_ok=True)
 
@@ -572,13 +598,12 @@ def put(*vargs):
                 current_block_name = f"{block_filename}_{i}"
                 if block_suffix:
                     current_block_name += block_suffix
-                # print(current_block_name)
                 current_block_path = block_root_path.joinpath(
                     current_block_name)
-                # print(current_block_path)
                 destination_block_name = f"{new_file_id}__{i}"
                 for j in range(args.REPLICATION_FACTOR):
-                    datanode_id = choose_datanode()
+                    datanode_id = choose_datanode(
+                        mode="hashing", block_path=current_block_path, replica_count=j)
                     if datanode_id is None:
                         print("Memory full")
                         del args.FILE_INFO[new_file_id]
@@ -586,12 +611,8 @@ def put(*vargs):
                         update_namenode_filesystem_info()
                         return
                     else:
-                        # print(args.PATH_TO_DATANODES,
-                        #   type(args.PATH_TO_DATANODES))
                         path_to_datanode_block = Path(
                             args.PATH_TO_DATANODES).joinpath(datanode_id).joinpath(destination_block_name)
-                        # update namenode info
-                        # update file info
                         shutil.copy(str(current_block_path),
                                     str(path_to_datanode_block))
                         log_datenode(
@@ -650,7 +671,7 @@ def run(*vargs):
     run_parser.add_argument(
         "--input", '-i', help="Input file path", type=str)
     run_parser.add_argument(
-        "--output", '-o', help="Output file path", type=str)
+        "--output", '-o', help="Output directory path", type=str)
     run_parser.add_argument(
         "--mapper", '-m', help="Mapper file path", type=str)
     run_parser.add_argument(
@@ -658,69 +679,77 @@ def run(*vargs):
     run_args = run_parser.parse_args(vargs)
 
     input_file_path = run_args.input
-    output_file_path = run_args.output
+    output_dir_path = run_args.output
     mapper_file_path = run_args.mapper
     reducer_file_path = run_args.reducer
 
-    if check_path_exists_in_hdfs(input_file_path) and Path(mapper_file_path).exists() and Path(reducer_file_path).exists():
-        file_id = get_file_id_from_hdfs_file_path(input_file_path)
-        if file_id is None:
-            print("Error: File not found.")
-            return False
-        else:
-            num_blocks = args.FILE_INFO[file_id]['num_blocks']
-            block_id = [f"{file_id}__{bid}" for bid in range(1, num_blocks+1)]
-            block_paths = dict()
-            for block_id in block_id:
-                datanode_id = get_datanode_id_from_block_id(block_id)
-                block_paths[block_id] = Path(
-                    args.PATH_TO_DATANODES).joinpath(datanode_id).joinpath(block_id)
-            if None in block_paths.values():
-                print("Error: Missing blocks or file not found.")
-                return False
-            else:
-                current_processes = list()
-                for process_index, block_path in enumerate(block_paths.values()):
-                    current_process = Process(
-                        target=run_mapper, args=(mapper_file_path, block_path, process_index))
-                    current_process.start()
-                    current_processes.append(current_process)
-                for current_process in current_processes:
-                    current_process.join()
-                current_processes.clear()
+    if not check_path_exists_in_hdfs(input_file_path):
+        print(f"Error: {input_file_path} not found")
+        return False
+    if not Path(mapper_file_path).exists():
+        print(f"Error: {mapper_file_path} not found")
+        return False
+    if not Path(reducer_file_path).exists():
+        print(f"Error: {reducer_file_path} not found")
+        return False
+    if check_path_exists_in_hdfs(output_dir_path):
+        print(f"Error: {output_dir_path} already present")
+        return False
 
-                output = sorted(SHARED_JOB_OUTPUT, key=lambda x: x[1])
-                output = '\n'.join(aggregate_and_sort(output))
-                run_reducer(reducer_file_path, output, output_file_path)
-    else:
-        # find which file is not found
+    file_id = get_file_id_from_hdfs_file_path(input_file_path)
+    if file_id is None:
         print("Error: File not found.")
         return False
+
+    num_blocks = args.FILE_INFO[file_id]['num_blocks']
+    block_id = [f"{file_id}__{bid}" for bid in range(1, num_blocks+1)]
+    block_paths = dict()
+    for block_id in block_id:
+        datanode_id = get_datanode_id_from_block_id(block_id)
+        block_paths[block_id] = Path(
+            args.PATH_TO_DATANODES).joinpath(datanode_id).joinpath(block_id)
+    if None in block_paths.values():
+        print("Error: Missing blocks or file not found.")
+        return False
+    else:
+        current_processes = list()
+        for process_index, block_path in enumerate(block_paths.values()):
+            current_process = Process(
+                target=run_mapper, args=(mapper_file_path, block_path, process_index))
+            current_process.start()
+            current_processes.append(current_process)
+
+        for current_process in current_processes:
+            current_process.join()
+        current_processes.clear()
+
+        output = sorted(SHARED_JOB_OUTPUT, key=lambda x: x[1])
+        output = '\n'.join(aggregate_and_sort(output))
+        run_reducer(reducer_file_path, output, output_dir_path)
+        return True
 
 
 def log_datenode(datanode_id, block_id, message):
     datanode_path = f"{datanode_id}_LOG.txt"
     date = str(datetime.datetime.now(IST))
     with open(Path(args.DATANODE_LOG_PATH).joinpath(datanode_path), "a") as f:
-        f.write(f"{date} {message.upper()} {block_id}\n")
-        f.close()
+        f.write(f"{date} {block_id} {message.upper()}\n")
 
 
 def log_namenode(message):
     date = str(datetime.datetime.now(IST))
     with open(Path(args.NAMENODE_LOG_PATH), "a") as f:
         f.write(f"{date} {message.upper()}\n")
-        f.close()
 
 
 load_args()
 # args.SYNC_PERIOD in below task loop
 
 
-@ TIMED_TASK_LOOP.job(interval=datetime.timedelta(seconds=5))
+@ TIMED_TASK_LOOP.job(interval=datetime.timedelta(seconds=60))
 def update_namenode():
     check_and_revive_primary_namenode()
-    log_namenode(f"SYNC")
+    log_namenode("SYNC")
     # check_and_revive_datanodes()
     update_namenode_datanode_info_local()
     update_namenode_block_info_local()
@@ -729,7 +758,6 @@ def update_namenode():
 
 
 TIMED_TASK_LOOP.start()
-
 
 command_map = {
     "put": put,
@@ -757,10 +785,12 @@ def process_input(_input):
         log_namenode(components[0])
         # check_and_revive_datanodes()
         if len(components) == 1:
-            # log into namenode
             _function()
         else:
             _function(*components[1:])
+
+        if command_string == "format":
+            exit(0)
 
 
 while True:
@@ -768,6 +798,7 @@ while True:
     if _input.lower() == "q":
         TIMED_TASK_LOOP.stop()
         exit(0)
+
     try:
         process_input(_input)
     except Exception as error_message:
--- todo.md
+++ todo.md
@@ -1,10 +1,7 @@
-- On rm- remove blocks from datanode, update block_info, update datanode_info
 - Handle deleted data nodes using check and revive function - check data node, each datanode, contents of each
-- check DFS can be loaded
-- Output directory for run
-- Hashing to choose data node
-
-- It should verify if the DFS can be loaded, by verifing the paths of the Data Nodes and Name Nodes. It must ensure and perform basic checks such as (but not limited to):
+- rmdir del from block info, datanode info, file info
+- Check DFS can be loaded
+    - It should verify if the DFS can be loaded, by verifing the paths of the Data Nodes and Name Nodes. It must ensure and perform basic checks such as (but not limited to):
     - Metadata contents about the Data Nodes stored by the Name Node exist and are correct
     - Ensure that each Data Node contains all the blocks it should contain
     - Ensure that the Name Node contains the correct mapping of blocks to Data Nodes
